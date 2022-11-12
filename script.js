89edd6ab1339e9aefd259a1b69e118cec74ddeff
DvK42 <joan.llan8s@gmail.com>
2022-11-12 20:08:16 +0100

    add change in color while hovered for grid elements

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const grid = document.querySelector("#grid");
+
+function onLeave(e) {
+  selectedDiv = e.path[0];
+  selectedDiv.classList.toggle("hovered");
+}
+
+function onEnter(e) {
+  selectedDiv = e.path[0];
+  selectedDiv.classList.toggle("hovered");
+}
+
 for (let i = 0; i < 16; i++) {
   let myDiv = document.createElement("div");
   myDiv.setAttribute("class", "gridElement");
+  myDiv.addEventListener("mouseenter", onEnter);
+  myDiv.addEventListener("mouseleave", onLeave);
   grid.appendChild(myDiv);
 }
