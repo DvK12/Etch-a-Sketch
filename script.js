0f8ee5cb2dc87da2cc5a0995f5d43f483d3a8186
DvK42 <joan.llan8s@gmail.com>
2022-11-12 20:23:39 +0100

    add button to input squares per side

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const grid = document.querySelector("#grid");
+const myButton = document.createElement("button");
+
+myButton.textContent =
+  "Click me to input how many squares you want on each side";
+myButton.addEventListener("click", onButtonClick);
+document.body.insertBefore(myButton, grid);
+
+function onButtonClick(e) {
+  let numberSquares = window.prompt(
+    "Input the number of squares you want, needs to be maximmum 100:"
+  );
+  if (numberSquares > 100) {
+    return;
+  } else {
+    generateGrid(numberSquares);
+  }
+}
 
 function onLeave(e) {
   selectedDiv = e.path[0];
@@ -10,10 +27,18 @@ function onEnter(e) {
   selectedDiv.classList.toggle("hovered");
 }
 
-for (let i = 0; i < 16; i++) {
-  let myDiv = document.createElement("div");
-  myDiv.setAttribute("class", "gridElement");
-  myDiv.addEventListener("mouseenter", onEnter);
-  myDiv.addEventListener("mouseleave", onLeave);
-  grid.appendChild(myDiv);
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+function generateGrid(numberSquares) {
+  removeAllChildNodes(grid);
+  for (let i = 0; i < numberSquares * numberSquares; i++) {
+    let myDiv = document.createElement("div");
+    myDiv.setAttribute("class", "gridElement");
+    myDiv.addEventListener("mouseenter", onEnter);
+    myDiv.addEventListener("mouseleave", onLeave);
+    grid.appendChild(myDiv);
+  }
 }
